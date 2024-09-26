53878cc1344e137369d7c4372cc772a1f10e807b
unknown <shaimazerin46@gmail.com>
2024-09-26 11:11:09 +0600

    send login to home page

--- js/login.js
+++ js/login.js
@@ -4,19 +4,20 @@
 document.getElementById('log_btn').addEventListener('click', function(event){
     //step 2: prevent default loading
     event.preventDefault();
-    console.log('login button clicked')
+    // console.log('login button clicked')
 // step 3: get the phon=e number value
     const phoneInput = document.getElementById('phone_inp').value
     
     // step 4: get pin number value
     const pinInput = document.getElementById('pin_inp').value
-    console.log(phoneInput, pinInput)
+    // console.log(phoneInput, pinInput)
 // this is temporary
 // step 5: validation
     if(phoneInput === '5' && pinInput === '1234')
     {
-        console.log('login successfull')
+        console.log('login successfull');
         // step 6: allow user to use the website
+        window.location.href = '/home.html';
     }
     else{
         console.log('something wrong')
